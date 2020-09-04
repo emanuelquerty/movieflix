d5d36f510b5ca535dcd18bac0b4bb3fc2a5d4a85
emanuelquerty <emanuelinacio1@hotmail.com>
2020-09-03 17:02:57 -0700

    Updates

---
+++ precache-manifest.689c48095a489c8186b9061ba51aae6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6062ba494c0a36666c2be9d47c6f49e3",
+    "url": "/movieflix/index.html"
+  },
+  {
+    "revision": "fb2a5b07e0afe5abc6d8",
+    "url": "/movieflix/static/css/main.188a2d6a.chunk.css"
+  },
+  {
+    "revision": "fb4f2c3aa7ba05558688",
+    "url": "/movieflix/static/js/2.2a0e6793.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movieflix/static/js/2.2a0e6793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2a5b07e0afe5abc6d8",
+    "url": "/movieflix/static/js/main.ec42a26c.chunk.js"
+  },
+  {
+    "revision": "f4803bf3d7dcba0eb29c",
+    "url": "/movieflix/static/js/runtime-main.3ffea526.js"
+  }
+]);
